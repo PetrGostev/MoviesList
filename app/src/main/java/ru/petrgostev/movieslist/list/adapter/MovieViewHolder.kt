bbc3b3a840c8147500279e79fd696899824c5083
PetrGostev <barsik@gmail.competrgostev@yandex.ru>
2021-12-22 11:27:38 +0300

    ViewHolderMovieBinding

--- app/src/main/java/ru/petrgostev/movieslist/list/adapter/MovieViewHolder.kt
+++ app/src/main/java/ru/petrgostev/movieslist/list/adapter/MovieViewHolder.kt
@@ -1,25 +1,18 @@
 package ru.petrgostev.movieslist.list.adapter
 
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import ru.petrgostev.movieslist.R
+import ru.petrgostev.movieslist.databinding.ViewHolderMovieBinding
 import ru.petrgostev.movieslist.list.ViewItem.MovieViewItem
 
 
-class MovieViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    private val poster: ImageView = itemView.findViewById(R.id.poster)
-    private val name: TextView = itemView.findViewById(R.id.name)
-    private val description: TextView = itemView.findViewById(R.id.description)
+class MovieViewHolder (val binding: ViewHolderMovieBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(item: MovieViewItem) {
-
-        poster.load(item.src)
-
-        name.text = item.name
-        description.text = item.description
+        binding.run {
+            poster.load(item.src)
+            name.text = item.name
+            description.text = item.description
+        }
     }
 }
--- app/src/main/java/ru/petrgostev/movieslist/list/adapter/MovieViewsAdapter.kt
+++ app/src/main/java/ru/petrgostev/movieslist/list/adapter/MovieViewsAdapter.kt
@@ -3,20 +3,17 @@ package ru.petrgostev.movieslist.list.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
-import ru.petrgostev.movieslist.R
+import ru.petrgostev.movieslist.databinding.ViewHolderMovieBinding
 import ru.petrgostev.movieslist.list.ViewItem.MovieViewItem
 
 
 class MovieViewsAdapter () : PagingDataAdapter<MovieViewItem, MovieViewHolder>(MovieDiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        return MovieViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.view_holder_movie,
-                parent,
-                false
-            )
-        )
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ViewHolderMovieBinding.inflate(layoutInflater, parent, false)
+
+        return MovieViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
